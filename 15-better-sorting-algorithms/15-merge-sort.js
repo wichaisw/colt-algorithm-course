c9c0d781660124370f5f7ebdb6f693c7fa6d6f0a
wichaisw <wichai.sw19@gmail.com>
2021-12-09 13:47:29 +0700

    feat: merge function

--- 15-better-sorting-algorithms/15-merge-sort.js
+++ 15-better-sorting-algorithms/15-merge-sort.js
@@ -1,78 +1,72 @@
 // my frist attempts
 function myMerge(arr1, arr2) {
-  let sortedArray = [];
+  if(arr1.length === 0) return arr2;
+  if(arr2.length === 0) return arr1;
+
+  let mergedArr = [];
   let i = 0;
   let j = 0;
 
-  while(i < arr1.length || j < arr2.length) {
-    if(arr1[i] <= arr2[j] || j >= arr2.length) {
-      sortedArray.push(arr1[i]);
+  let minArr = [];
+  let maxArr = []; 
+
+  if(arr1[arr1.length - 1] > arr2[arr2.length - 1]) {
+    maxArr = arr1;
+    minArr = arr2;
+  } else {
+    maxArr = arr2;
+    minArr = arr1;
+  }
+  
+  while(i < minArr.length) {
+    if(minArr[i] < maxArr[j]) {
+      mergedArr.push(minArr[i]);
       i++;
     } else {
-      sortedArray.push(arr2[j]);
+      mergedArr.push(maxArr[j]);
       j++;
-    }
-
-    console.log(i)
-    console.log(j)
-    console.log(sortedArray)
+    }    
   }
 
-  return sortedArray;
+  while(j < maxArr.length) {
+    mergedArr.push(maxArr[j]);
+    j++;
+  }
+
+  return mergedArr;
 }
 
+// colt's merge
 
 function merge(arr1, arr2) {
   let results = [];
   let i = 0;
   let j = 0;
+
   while(i < arr1.length && j < arr2.length) {
-    if(arr2[j] > arr1[i]) {
+    if(arr1[i] < arr2[j]) {
       results.push(arr1[i]);
       i++;
     } else {
       results.push(arr2[j]);
       j++;
-    } 
+    }
   }
 
   while(i < arr1.length) {
     results.push(arr1[i]);
     i++;
-  } 
+  }
+
   while(j < arr2.length) {
     results.push(arr2[j]);
     j++;
-  } 
-
+  }
 
   return results;
 }
 
-// my frist attempts
-const myMergeSort = (arr) => {
-  let whole = [];
-  
-  function split(arr) {
-    if(arr.length <= 1) {
-      return whole;
-    }
-    let arr1 = [];
-    let arr2 = [];
-    let middle = Math.floor(arr.length / 2);
-    arr1 = arr.slice(0, middle);
-    arr2 = arr.slice(middle + 1, arr.length);    
-    whole.push(arr1, arr2);
-    split(arr1);
-  }
-
-  split(arr);
-
-  // if(arr1.length <= 1 && arr2.length <= 1) {
-
-  // }
-
-
-}
-
-console.log(merge([5, 12, 22, 23, 34, 67, 90, 109],  [-65, -4, -1, 1, 3, 6, 20, 34, 34, 55, 78, 90]))
+// let result = myMerge([5, 12, 22, 23, 34, 67, 90, 109], [-65, -4, -1, 1, 3, 6, 20, 34, 34, 55, 78, 90]) 
+let result = merge([1,10,50], [2,14,99,100]);
+console.log(result)
+console.log(result.length)
