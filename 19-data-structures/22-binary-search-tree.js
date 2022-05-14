367df97a51c6e0781a362de903dd1ce45a6e7762
wichaisw <wichai.sw19@gmail.com>
2022-05-14 13:49:22 +0700

    feat: tree traversal: breadth first search

--- 19-data-structures/22-binary-search-tree.js
+++ 19-data-structures/22-binary-search-tree.js
@@ -81,15 +81,32 @@ class BinarySearchTree {
     //   }
     // }
   }
+
+  // ANCHOR Breadth First Search
+  BFS() {
+    // use arra queue for simplicity purpose
+    const data = [], queue = [];
+    let node = this.root;
+
+    queue.push(this.root);
+    while(queue.length) {
+      // FIFO
+      node = queue.shift();
+      data.push(node.value);
+      if(node.left) queue.push(node.left);
+      if(node.right) queue.push(node.right);
+    }
+
+    return data;
+  }
 }
 
 const tree = new BinarySearchTree();
 tree.insert(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(11);
-tree.insert(2);
-tree.insert(16);
-tree.insert(7);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
 
-console.log(tree.find(5))
+console.log(tree.BFS());
